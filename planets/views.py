d5497cf76cbb770525aeaebeef0b4179d8f62491
Steph Hjartarson <stephanie.hja@gmail.com>
2022-11-22 21:33:06 +0000

    Create index.html file

--- planets/views.py
+++ planets/views.py
@@ -3,15 +3,15 @@ from django.views import generic
 from .models import Planet
 
 
-class PlanetList(generic.ListView):
-    model = Planet
-    queryset = Planet.objects.order_by('-created_on')
-    template_name = 'planets_list.html'
+# class PlanetList(generic.ListView):
+#     model = Planet
+#     queryset = Planet.objects.order_by('-created_on')
+#     template_name = 'planets_list.html'
 
 
-# def get_planet_list(request):
-#     return render(request, 'planets/planets_list.html')
+def get_planet_list(request):
+    return render(request, 'planets/planets_list.html')
 
 
-# def home(request):
-#     return render(request, 'planets/base.html')
+def home(request):
+    return render(request, 'planets/base.html')
