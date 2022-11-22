770a23d60fbf62a5a5f15b1c3394c84de520dd79
Steph Hjartarson <stephanie.hja@gmail.com>
2022-11-22 21:12:51 +0000

    Create class-based view for planet list

--- planets/views.py
+++ planets/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+
 
 # Create your views here.
+
+class PlanetList(generic.ListView):
+    model = Planet
+    queryset = Planet.objects.filter(status=1).order_by('-created_on')
+    template_name = 'planets_list.html'
 
 
 def get_planet_list(request):
