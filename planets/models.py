129a0faf47fc5fdf3d899ac4a5936d1d1b125959
Steph Hjartarson <stephanie.hja@gmail.com>
2022-11-22 21:29:59 +0000

    Create planet object

--- planets/models.py
+++ planets/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+
+
+class Planet(models.Model):
+    name = models.CharField(max_length=200, unique=True),
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="planets"),
+    fully_explored = models.BooleanField(default=False),
+    turian_insignia = models.BooleanField(default=False),
+    asari_writing = models.BooleanField(default=False),
+    prothean_disc = models.BooleanField(default=False),
+    mineral = models.BooleanField(default=False),
+    medallion = models.BooleanField(default=False),
+    notes = models.TextField(blank=True),
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.name
--- planets/views.py
+++ planets/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Planet
 
-
-# Create your views here.
 
 class PlanetList(generic.ListView):
     model = Planet
-    queryset = Planet.objects.filter(status=1).order_by('-created_on')
+    queryset = Planet.objects.order_by('-created_on')
     template_name = 'planets_list.html'
 
 
-def get_planet_list(request):
-    return render(request, 'planets/planets_list.html')
+# def get_planet_list(request):
+#     return render(request, 'planets/planets_list.html')
 
 
-def home(request):
-    return render(request, 'planets/base.html')
+# def home(request):
+#     return render(request, 'planets/base.html')
