d4f2daa09a4772340a8039dca1a9140196ab083f
Steph Hjartarson <stephanie.hja@gmail.com>
2022-11-27 15:55:05 +0000

    Update planet list to only show active user's planets

--- planets/views.py
+++ planets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic import TemplateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from planets.models import Planet
 
 
@@ -8,7 +9,9 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class PlanetList(generic.ListView):
+class PlanetList(LoginRequiredMixin, ListView):
     model = Planet
-    queryset = Planet.objects.order_by('-created_on')
     template_name = 'planet_list.html'
+
+    def get_queryset(self):
+        return Planet.objects.filter(user=self.request.user).order_by('-created_on')
