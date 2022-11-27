e1595e9eedb57616be05adaf56cf4398eb30f534
Steph Hjartarson <stephanie.hja@gmail.com>
2022-11-27 13:59:59 +0000

    Install allauth and wire up links

--- planet_tracker/settings.py
+++ planet_tracker/settings.py
@@ -41,11 +41,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'planets',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -81,16 +92,16 @@ WSGI_APPLICATION = 'planet_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
--- planet_tracker/urls.py
+++ planet_tracker/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from planets.views import IndexView, PlanetList
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name="admin"),
-    path('', IndexView.as_view(), name="home"),
-    path('planet_list/', PlanetList.as_view(), name="planet_list"),
+    path('admin/', admin.site.urls, name='admin'),
+    path('', IndexView.as_view(), name='home'),
+    path('planet_list/', PlanetList.as_view(), name='planet_list'),
+    path('accounts/', include('allauth.urls')),
 ]
