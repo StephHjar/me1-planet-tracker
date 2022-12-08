85f391a809dae88038c037cc86a9052ef6f14917
Steph Hjartarson <stephanie.hja@gmail.com>
2022-12-08 23:37:27 +0000

    Load cloudinary in base.html

--- planets/urls.py
+++ planets/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('planet_list/', views.PlanetList.as_view(), name='planet_list'),
     path('add_planet/', views.AddPlanet.as_view(), name='add_planet'),
-    path('edit_planet/<pk>', views.EditPlanet.as_view(),
+    path('planet/edit/<int:pk>', views.EditPlanet.as_view(),
          name='planet_form'),
-    path('delete_planet/<int:pk>', views.DeletePlanet.as_view(),
+    path('planet/delete/<int:pk>', views.DeletePlanet.as_view(),
          name='delete_planet'),
 ]
