e1260e02a3958dc465ba590e324d545371c7e5d3
Steph Hjartarson <stephanie.hja@gmail.com>
2022-12-15 20:54:19 +0000

    Complete jest testing

--- static/js/tests/script.test.js
+++ static/js/tests/script.test.js
@@ -2,53 +2,52 @@
  * @jest-environment jsdom
  */
 
-// Import the verifyPassword function that you want to test
+// Import the verifyPassword function to be tested
 const {
     verifyPassword
 } = require('../script');
 
-test('verifyPassword should return a window alert if the password field is blank', () => {
-    // Create a mock for document.getElementById
-    const mockGetElementById = jest.spyOn(document, 'getElementById');
-    // Tell the mock to return a fake password input element when it is called
-    mockGetElementById.mockReturnValue({
-        value: ''
-    });
+// Create a mock for document.getElementById
+const mockGetElementById = jest.spyOn(document, 'getElementById');
 
+beforeAll(() => {
     // Create a mock for window.alert
     const mockAlert = jest.fn();
     // Tell the mock to do nothing when it is called
     mockAlert.mockImplementation(() => {});
     // Override the global window.alert function with the mock
     global.window.alert = mockAlert;
+});
 
-    // Call the verifyPassword function and assert that it returns true
-    expect(verifyPassword()).toBe(false);
-
+afterAll(() => {
     // Restore the original document.getElementById and window.alert functions
     mockGetElementById.mockRestore();
     global.window.alert = alert;
 });
 
-test('verifyPassword should return no window alert if the password is between 8 and 15 characters', () => {
-    // Create a mock for document.getElementById
-    const mockGetElementById = jest.spyOn(document, 'getElementById');
-    // Tell the mock to return a fake password input element when it is called
-    mockGetElementById.mockReturnValue({
-        value: 'test-password'
+describe("verifyPassword function returns the correct alerts", () => {
+    test("verifyPassword should return an alert if the password field is blank", () => {
+        mockGetElementById.mockReturnValue({
+            value: ''
+        });
+        expect(verifyPassword()).toBe(false);
     });
-
-    // Create a mock for window.alert
-    const mockAlert = jest.fn();
-    // Tell the mock to do nothing when it is called
-    mockAlert.mockImplementation(() => {});
-    // Override the global window.alert function with the mock
-    global.window.alert = mockAlert;
-
-    // Call the verifyPassword function and assert that it returns true
-    expect(verifyPassword()).toBe(true);
-
-    // Restore the original document.getElementById and window.alert functions
-    mockGetElementById.mockRestore();
-    global.window.alert = alert;
+    test("verifyPassword should return an alert if the password is less than 8 characters", () => {
+        mockGetElementById.mockReturnValue({
+            value: "short"
+        });
+        expect(verifyPassword()).toBe(false);
+    });
+    test("verifyPassword should return an alert if the password is more than 15 characters", () => {
+        mockGetElementById.mockReturnValue({
+            value: "passwordistoolong"
+        });
+        expect(verifyPassword()).toBe(false);
+    });
+    test('verifyPassword should return no alert if the password is between 8 and 15 characters', () => {
+        mockGetElementById.mockReturnValue({
+            value: "test-password"
+        });
+        expect(verifyPassword()).toBe(true);
+    });
 });
