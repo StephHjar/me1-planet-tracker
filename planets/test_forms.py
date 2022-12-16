4a9775c7fa438e70bcba1890618b43ee24ccdac7
Steph Hjartarson <stephanie.hja@gmail.com>
2022-12-16 16:58:26 +0000

    Complete unit tests for add planet form

--- planets/test_forms.py
+++ planets/test_forms.py
@@ -2,11 +2,8 @@ import unittest
 from django.test import TestCase
 from .forms import AddPlanetForm
 
-        # fields = 'asari_writing', 'prothean_disc', 'mineral', 'medallion',
-        #           'notes',)
 
-
-class TestPlanetForm(TestCase):
+class TestAddPlanetForm(TestCase):
     def test_name_is_required(self):
         form = AddPlanetForm({'name': ''})
         self.assertFalse(form.is_valid())
@@ -21,4 +18,24 @@ class TestPlanetForm(TestCase):
 
     def test_turian_insignia_is_not_required(self):
         form = AddPlanetForm({'name': 'Jartar', 'turian_insignia': False})
+        self.assertTrue(form.is_valid())
+
+    def test_asari_writing_is_not_required(self):
+        form = AddPlanetForm({'name': 'Wermani', 'asari_writing': False})
+        self.assertTrue(form.is_valid())
+
+    def test_prothean_disc_is_not_required(self):
+        form = AddPlanetForm({'name': 'Luna', 'prothean_disc': False})
+        self.assertTrue(form.is_valid())
+
+    def test_mineral_is_not_required(self):
+        form = AddPlanetForm({'name': 'Inti', 'mineral': False})
+        self.assertTrue(form.is_valid())
+
+    def test_medallion_is_not_required(self):
+        form = AddPlanetForm({'name': 'Klensal', 'medallion': False})
+        self.assertTrue(form.is_valid())
+
+    def test_notes_are_not_required(self):
+        form = AddPlanetForm({'name': 'Paravin', 'notes': ''})
         self.assertTrue(form.is_valid())
