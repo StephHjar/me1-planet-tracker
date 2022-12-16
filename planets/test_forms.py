2cc9509938f1bc92fe5544702e47d6054afc8e79
Steph Hjartarson <stephanie.hja@gmail.com>
2022-12-16 17:18:29 +0000

    Add tests for EditPlanetForm

--- planets/test_forms.py
+++ planets/test_forms.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .forms import AddPlanetForm
+from .forms import AddPlanetForm, EditPlanetForm
 
 
 class TestAddPlanetForm(TestCase):
@@ -47,6 +47,47 @@ class TestAddPlanetForm(TestCase):
         form = AddPlanetForm()
         self.assertEqual(form.Meta.fields, ('name',
                                             'fully_explored',
+                                            'turian_insignia',
+                                            'asari_writing',
+                                            'prothean_disc',
+                                            'mineral',
+                                            'medallion',
+                                            'notes'))
+
+
+class TestEditPlanetForm(TestCase):
+
+    def test_fully_explored_is_not_required(self):
+        form = EditPlanetForm({'name': 'Noveria'})
+        self.assertTrue(form.is_valid())
+
+    def test_turian_insignia_is_not_required(self):
+        form = EditPlanetForm({'name': 'Jartar'})
+        self.assertTrue(form.is_valid())
+
+    def test_asari_writing_is_not_required(self):
+        form = EditPlanetForm({'name': 'Wermani'})
+        self.assertTrue(form.is_valid())
+
+    def test_prothean_disc_is_not_required(self):
+        form = EditPlanetForm({'name': 'Luna'})
+        self.assertTrue(form.is_valid())
+
+    def test_mineral_is_not_required(self):
+        form = EditPlanetForm({'name': 'Inti'})
+        self.assertTrue(form.is_valid())
+
+    def test_medallion_is_not_required(self):
+        form = EditPlanetForm({'name': 'Klensal'})
+        self.assertTrue(form.is_valid())
+
+    def test_notes_are_not_required(self):
+        form = EditPlanetForm({'name': 'Paravin'})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = EditPlanetForm()
+        self.assertEqual(form.Meta.fields, ('fully_explored',
                                             'turian_insignia',
                                             'asari_writing',
                                             'prothean_disc',
